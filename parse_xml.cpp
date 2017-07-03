c9b61e3b568d46e9d8a43c7c63df79668b60bf8b
yangwj <yangwj@spreadst.com>
2017-07-03 19:09:33 +0800

    update code

--- parse_xml.cpp
+++ parse_xml.cpp
@@ -29,155 +29,141 @@
 #include "parse_xml.h"
 
 static cert_elems_t cert_elems[] = {
-    {ELEMS_XML_VERSION, "xml version", "", nullptr},
-    {ELEMS_KEYBOXS_NUM, "<NumberOfKeyboxes>", "</NumberOfKeyboxes>", nullptr},
-    {ELEMS_DEVICE_ID, "Keybox DeviceID", "", nullptr},
-    {ELEMS_ECC_KEY_ALGO, "Key algorithm", "", nullptr},
-    {ELEMS_ECC_KEY_FORMAT, "PrivateKey format", "", nullptr},
-    {ELEMS_ECC_PRIV_KEY, "BEGIN EC PRIVATE KEY", "END EC PRIVATE KEY", nullptr},
-    {ELEMS_ECC_CERT_NUM, "<NumberOfCertificates>", "</NumberOfCertificates>",
-     nullptr},
-    {ELEMS_ECC_CERT_FORMAT, "Certificate format", "", nullptr},
-    {ELEMS_ECC_ATTEST_CERT, "BEGIN CERTIFICATE", "END CERTIFICATE", nullptr},
-    {ELEMS_ECC_ROOT_CERT, "BEGIN CERTIFICATE", "END CERTIFICATE", nullptr},
+    {ELEMS_XML_VERSION, "xmlversion","", nullptr},
+    {ELEMS_KEYBOXS_NUM, "<NumberOfKeyboxes>","</NumberOfKeyboxes>", nullptr},
+    {ELEMS_DEVICE_ID, "KeyboxDeviceID","", nullptr},
+    {ELEMS_KEY_ALGO, "Keyalgorithm","</Key>", nullptr},
+    {ELEMS_KEY_FORMAT, "PrivateKeyformat","</PrivateKey>", nullptr},
+    {ELEMS_PRIV_KEY, "PRIVATEKEY","END", nullptr},
+    {ELEMS_CERT_NUM, "<NumberOfCertificates>", "</NumberOfCertificates>",nullptr},
+    {ELEMS_CERT_FORMAT, "<Certificate","</Certificate>", nullptr},
+    {ELEMS_ATTEST_CERT, "BEGINCERTIFICATE","ENDCERTIFICATE", nullptr},
+    {ELEMS_ROOT_CERT, "BEGINCERTIFICATE","ENDCERTIFICATE",nullptr},
+    {ELEMS_MAX_ITERM, "Unknown","",nullptr}};
 
-    {ELEMS_RSA_KEY_ALGO, "<Key algorithm", "", nullptr},
-    {ELEMS_RSA_KEY_FORMAT, "PrivateKey format", "", nullptr},
-    {ELEMS_RSA_PRIV_KEY, "BEGIN RSA PRIVATE KEY", "END RSA PRIVATE KEY",
-     nullptr},
-    {ELEMS_RSA_CERT_NUM, "<NumberOfCertificates>", "</NumberOfCertificates>",
-     nullptr},
-    {ELEMS_RSA_CERT_FORMAT, "Certificate", "", nullptr},
-    {ELEMS_RSA_ATTEST_CERT, "BEGIN CERTIFICATE", "END CERTIFICATE", nullptr},
-    {ELEMS_RSA_ROOT_CERT, "BEGIN CERTIFICATE", "END CERTIFICATE", nullptr},
-    {ELEMS_MAX_ITERM, "Unknown", "", nullptr}};
-
-ParseXml::ParseXml() : kRsaAttestKeySize(0) {
-  kRsaAttestKey = (char *)malloc(1024);
-  kRsaAttestKeySize = 0;
-  kRsaAttestCert = (char *)malloc(1024);
-  kRsaAttestCertSize = 0;
-  kRsaAttestRootCert = (char *)malloc(1024);
-  kRsaAttestRootCertSize = 0;
-  kEcAttestKey = (char *)malloc(1024);
-  kEcAttestKeySize = 0;
-  kEcAttestCert = (char *)malloc(1024);
-  kEcAttestCertSize = 0;
-  kEcAttestRootCert = (char *)malloc(1024);
-  kEcAttestRootCertSize = 0;
-
-	memset(devID,0, sizeof(devID));
-	memset(ecc_name,0, sizeof(ecc_name));
-	memset(ecc_key_f,0, sizeof(ecc_key_f));
-	memset(ecc_cert_f,0, sizeof(ecc_cert_f));
-	memset(rsa_key_f,0, sizeof(rsa_key_f));
-	memset(rsa_cert_f,0, sizeof(rsa_cert_f));
-
-  read_xml_from_rpmb();
-  parse_xml_elems();
-	switch_Attest_Context();
+    ParseXml::ParseXml(){
+    read_xml_from_rpmb();
 }
 ParseXml::~ParseXml() {
-  free(xml_data);
-  free(kRsaAttestKey);
-  free(kRsaAttestCert);
-  free(kRsaAttestRootCert);
-  free(kEcAttestKey);
-  free(kEcAttestCert);
-  free(kEcAttestRootCert);
+free(xml_data);
 }
 
 bool ParseXml::read_xml_from_rpmb(void) {
-
-  int fd = open(CERT_FILE_NAME, 'r');
-  xml_size = lseek(fd, 0, SEEK_END);
-  xml_data = (char *)malloc(xml_size);
-  lseek(fd, 0, SEEK_SET);
-  read(fd, xml_data, xml_size);
-  close(fd);
-  elem_end = xml_data;
-  elem = xml_data;
-  return true;
+    char *buf;
+    int fd = open(CERT_FILE_NAME, O_RDONLY);
+    xml_size = lseek(fd, 0, SEEK_END);
+    xml_data = (char *)malloc(xml_size);
+    buf = (char *)malloc(xml_size);
+    lseek(fd, 0, SEEK_SET);
+    int rs=read(fd, buf, xml_size);
+    if(rs < 0)return rs;
+    close(fd);
+    skip_sp(buf,xml_data);
+    free(buf);
+    key_elem=xml_data;
+    elem=key_elem;
+    elem_end=key_elem;
+    return true;
+}
+bool ParseXml::write_data2file(const char* filename,const char* data) {
+    char path[64] ={0};
+    sprintf(path,"save/Attest.%s",filename);
+    if(access("save",0) !=0){
+        mkdir("save",0755);
+    }
+    FILE* fd = fopen(path,"w+");
+    fseek(fd, 0, SEEK_SET);
+    fwrite(data,strlen(data),sizeof(char),fd);
+    fclose(fd);
+    return true;
 }
 
-void ParseXml::get_new_line(void) {
-  elem = elem_end;
-  while (*elem_end != '>')
+void ParseXml::skip_sp(char* in,char*out) {
+    while (*in++){
+        if(*in != 32&&*in != 10&&*in != 13)
+           *out++=*in;
+    }
+}
+
+void ParseXml::get_new_line() {
+    elem = elem_end;
+    while (*elem_end != '>')
+        elem_end++;
     elem_end++;
-  elem_end++;
 }
 
 char *ParseXml::strXmlStart(const char *s1, const char *start,
                             const char *end) {
-  int l1, l2;
+    int l1, l2;
 
-  l1 = strlen((const char *)s1);
-  if (!l1)
-    return (char *)start;
-  l2 = end - start;
-  while (l2 >= l1) {
-    l2--;
-    if (!memcmp(start, s1, l1)) {
-      start += l1;
-      return (char *)start;
+    l1 = strlen((const char *)s1);
+    if (!l1)
+        return (char *)start;
+    l2 = end - start;
+    while (l2 >= l1) {
+        l2--;
+        if (!memcmp(start, s1, l1)) {
+            start += l1;
+            return (char *)start;
+        }
+        start++;
     }
-    start++;
-  }
-  start += l1 - 1;
-  return NULL;
+    start += l1 - 1;
+    return NULL;
 }
 
 char *ParseXml::strXmlEnd(const char *s1, const char *start, const char *end) {
   int l1, l2;
 
-  l1 = strlen((const char *)s1);
-  if (!l1)
-    return (char *)start;
-  l2 = end - start;
-  while (l2 >= l1) {
-    l2--;
-    if (!memcmp(start, s1, l1))
-      return (char *)start;
-    start++;
-  }
-  return NULL;
-}
-void dumpString(char *start, char *end) {
-  printf("start=%p end=%p length = %d\n",start,end, end-start);
-  while (start < end) {
-    printf("%c", *start++);
-  }
-  printf("\n");
-}
-void ParseXml::parse_xml_elems(void) {
-  char *start;
-  char *end;
-    char type = 0;
-
-    for (type = 0; type < (char)ELEMS_MAX_ITERM; type++) {
-
-      do {
-        get_new_line();
-        start = strXmlStart(cert_elems[type].tag_begin, elem, elem_end);
-      } while (start == NULL);
-      end = elem_end;
-      if (strlen(cert_elems[type].tag_end) != 0) {
-        do {
-          get_new_line();
-          end = strXmlEnd(cert_elems[type].tag_end, start, elem_end);
-        } while (start == NULL);
-      }
-      get_cert_data((xml_elems_e)type, start, end);
+    l1 = strlen((const char *)s1);
+    if (!l1)
+        return (char *)start;
+    l2 = end - start;
+    while (l2 >= l1) {
+        l2--;
+        if (!memcmp(start, s1, l1))
+            return (char *)start;
+        start++;
     }
+    return NULL;
+}
+void dumpString(const char *start, const char *end) {
+    printf("start=%p end=%p length = %ld\n",start,end, end-start);
+    while (start < end) {
+        printf("%c", *start++);
+    }
+    printf("\n");
+}
+void ParseXml::parse_xml_elems(char type) {
+    const char *start=NULL;
+    const char *end=NULL;
+    elem=key_elem;
+    elem_end=key_elem;
+    do {
+        get_new_line();
+        if(elem_end >= xml_data+xml_size)break;
+        start = strXmlStart(cert_elems[type].tag_begin, elem, elem_end);
+    } while (start == NULL);
+    end = elem_end;
+    if (start!=NULL&&strlen(cert_elems[type].tag_end) != 0) {
+        do {
+            get_new_line();
+            if(elem_end >= xml_data+xml_size)break;
+            end = strXmlEnd(cert_elems[type].tag_end, start, elem_end);
+        } while (end== NULL);
+    }
+    if(start!=NULL && end!=NULL)
+        GetItemsValue((xml_elems_e)type, start, end);
 }
 void ParseXml::get_elems_value(const char *begin, const char *end,
                                char *value) {
   while (begin < end) {
-    if (*begin == '<' || *begin == '>' || *begin == '=' || *begin == '\"' ||
+    if (*begin == '<' || *begin == '=' || *begin == '\"' ||
         *begin == '?'||((*begin&0x80) == 0x80)) {
       begin++;
       continue;
     }
+    if(*begin =='>')break;
     *value++ = *begin++;
   }
 }
@@ -186,87 +172,83 @@ void ParseXml::get_elems_value(const char *begin, const char *end, char *value,
                                size_t *len) {
   int i = 0;
   while (begin < end) {
-    if (*begin == '<' || *begin == '>' || *begin == '-' || *begin == '\"'||((*begin&0x80) == 0x80)) {
+    if (*begin == '<' || *begin == '-' ||
+          *begin == '\"'||((*begin&0x80) == 0x80)) {
       begin++;
       continue;
     }
+    if(*begin =='>')break;
     *value++ = *begin++;
     i++;
   }
   *len = i;
 }
 
-bool ParseXml::get_cert_data(xml_elems_e type, const char *begin,
+bool ParseXml::GetItemsValue(xml_elems_e type, const char *begin,
                              const char *end) {
-  char num[2];
-  switch (type) {
-  case ELEMS_XML_VERSION:
-    get_elems_value(begin, end, version);
-    break;
-  case ELEMS_DEVICE_ID:
-    get_elems_value(begin, end, devID);
-    break;
-  case ELEMS_KEYBOXS_NUM:
-    get_elems_value(begin, end, num);
-    key_num = num[0] - '0';
-    break;
-  case ELEMS_ECC_KEY_ALGO:
-    get_elems_value(begin, end, ecc_name);
-    break;
-  case ELEMS_ECC_KEY_FORMAT:
-    get_elems_value(begin, end, ecc_key_f);
-    break;
-  case ELEMS_ECC_PRIV_KEY:
-    get_elems_value(begin, end, kEcAttestKey, &kEcAttestKeySize);
-    break;
-  case ELEMS_ECC_CERT_NUM:
-    get_elems_value(begin, end, num);
-    ecc_cert_num = num[0] - '0';
-    break;
-  case ELEMS_ECC_CERT_FORMAT:
-    get_elems_value(begin, end, rsa_key_f);
-    break;
-  case ELEMS_ECC_ATTEST_CERT:
-    get_elems_value(begin, end, kEcAttestCert, &kEcAttestCertSize);
-    break;
-  case ELEMS_ECC_ROOT_CERT:
-    get_elems_value(begin, end, kEcAttestRootCert, &kEcAttestRootCertSize);
-    break;
-  case ELEMS_RSA_KEY_ALGO:
-    get_elems_value(begin, end, rsa_name);
-    break;
-  case ELEMS_RSA_KEY_FORMAT:
-    get_elems_value(begin, end, rsa_key_f);
-    break;
-  case ELEMS_RSA_PRIV_KEY:
-    get_elems_value(begin, end, kRsaAttestKey, &kRsaAttestKeySize);
-    break;
-  case ELEMS_RSA_CERT_NUM:
-    get_elems_value(begin, end, num);
-    rsa_cert_num = num[0] - '0';
-    break;
-  case ELEMS_RSA_CERT_FORMAT:
-    get_elems_value(begin, end, rsa_cert_f);
-    break;
-  case ELEMS_RSA_ATTEST_CERT:
-    get_elems_value(begin, end, kRsaAttestCert, &kRsaAttestCertSize);
-    break;
-  case ELEMS_RSA_ROOT_CERT:
-    get_elems_value(begin, end, kRsaAttestRootCert, &kRsaAttestRootCertSize);
-    break;
-  default:
-    break;
-  }
-  return true;
+    memset(AttestBuf,0,sizeof(AttestBuf));
+    AttestSize=0;
+    switch (type) {
+        case ELEMS_KEYBOXS_NUM:
+        case ELEMS_CERT_NUM:
+            get_elems_value(begin, end, AttestBuf);
+            key_num =atoi(AttestBuf);
+            break;
+        case ELEMS_KEY_ALGO:
+        case ELEMS_CERT_FORMAT:
+        case ELEMS_KEY_FORMAT:
+        case ELEMS_DEVICE_ID:
+        case ELEMS_XML_VERSION:
+            get_elems_value(begin, end, AttestBuf);
+            break;
+        case ELEMS_PRIV_KEY:
+        case ELEMS_ATTEST_CERT:
+        case ELEMS_ROOT_CERT:
+            get_elems_value(begin, end, AttestBuf, &AttestSize);
+            break;
+       default:
+           break;
+    }
+    return true;
 }
-void ParseXml::switch_Attest_Context(void){
-	if(!memcmp(ecc_name,"ecdsa",strlen(ecc_name))){
-    printf("%s\n", ecc_name);
-	}
+void ParseXml::SaveXmlFiles(void){
+    parse_xml_elems(ELEMS_XML_VERSION);
+    printf("[ ELEMS_XML_VERSION ] %s\n",AttestBuf);
+    parse_xml_elems(ELEMS_DEVICE_ID);
+    printf("[ ELEMS_DEVICE_ID ] %s\n",AttestBuf);
+    parse_xml_elems(ELEMS_KEYBOXS_NUM);
+    printf("[ ELEMS_KEYBOXS_NUM ] %d\n",key_num);
+    for(int i=0;i<key_num;i++){
+        char algo[8]={0};
+        char filename[32]={0};
+        parse_xml_elems(ELEMS_KEY_ALGO);
+        printf("[ ELEMS_KEY_ALGO ] %s\n",AttestBuf);
+        memcpy(algo,AttestBuf,strlen(AttestBuf));
+        parse_xml_elems(ELEMS_PRIV_KEY);
+        printf("[ ELEMS_PRIV_KEY ] %s\n",AttestBuf);
+        sprintf(filename,"%s.prikey",algo);
+        write_data2file(filename,AttestBuf);
+        parse_xml_elems(ELEMS_CERT_NUM);
+        parse_xml_elems(ELEMS_KEYBOXS_NUM);
+        if(AttestBuf != NULL){
+            printf("[ ELEMS_CERT_NUM ] %d\n",key_num);
+            sprintf(filename,"%s.Certnum",algo);
+            write_data2file(filename,AttestBuf);
+        }
+        parse_xml_elems(ELEMS_ATTEST_CERT);
+        printf("[ ELEMS_ATTEST_CERT ] %s\n",AttestBuf);
+        sprintf(filename,"%s.attestcert",algo);
+        write_data2file(filename,AttestBuf);
+        parse_xml_elems(ELEMS_ROOT_CERT);
+        printf("[ ELEMS_ROOT_CERT ] %s\n",AttestBuf);
+        sprintf(filename,"%s.rootcert",algo);
+        write_data2file(filename,AttestBuf);
+        key_elem = strXmlStart(cert_elems[ELEMS_KEY_ALGO].tag_end,key_elem,xml_data+xml_size);
+        //printf("[ ELEMS_KEY_ALGO ] %s\n", key_elem);
+    }
 }
-
 int main() {
-  ParseXml op;
-   printf("%s\n", op.GetRsaAttestKey());
-  return 0;
+    ParseXml op;
+    op.SaveXmlFiles();
+    return 0;
 }
--- parse_xml.h
+++ parse_xml.h
@@ -18,125 +18,55 @@
 
 #pragma once
 
-#define ATTEST_ECC_DATA_MASK 0x2000
-#define ATTEST_RSA_DATA_MASK 0x3000
-#define CERT_FILE_NAME  "TrustyCertChains.xml"
+#define CERT_FILE_NAME  "data/TrustyCertChains.xml"
 #define nullptr (void*)0
 
 typedef enum xml_elems_{
-	ELEMS_XML_VERSION=0,
-	ELEMS_KEYBOXS_NUM,
-	ELEMS_DEVICE_ID,
-
-	ELEMS_ECC_KEY_ALGO,
-	ELEMS_ECC_KEY_FORMAT,
-	ELEMS_ECC_PRIV_KEY,
-	ELEMS_ECC_CERT_NUM,
-	ELEMS_ECC_CERT_FORMAT,
-	ELEMS_ECC_ATTEST_CERT,
-	ELEMS_ECC_ROOT_CERT,
-
-	ELEMS_RSA_KEY_ALGO,
-	ELEMS_RSA_KEY_FORMAT,
-	ELEMS_RSA_PRIV_KEY,
-	ELEMS_RSA_CERT_NUM,
-	ELEMS_RSA_CERT_FORMAT,
-	ELEMS_RSA_ATTEST_CERT,
-	ELEMS_RSA_ROOT_CERT,
-	ELEMS_MAX_ITERM,
+    ELEMS_XML_VERSION=0,
+    ELEMS_KEYBOXS_NUM,
+    ELEMS_DEVICE_ID,
+    ELEMS_KEY_ALGO,
+    ELEMS_KEY_FORMAT,
+    ELEMS_PRIV_KEY,
+    ELEMS_CERT_NUM,
+    ELEMS_CERT_FORMAT,
+    ELEMS_ATTEST_CERT,
+    ELEMS_ROOT_CERT,
+    ELEMS_MAX_ITERM,
 }xml_elems_e;
 
 typedef struct cert_elems_{
-	xml_elems_e type;
-	char tag_begin[32];
-	char tag_end[32];
-	void *data;
+    xml_elems_e type;
+    const char tag_begin[32];
+    const char tag_end[32];
+    void *data;
 }cert_elems_t;
 
 class ParseXml{
 public:
-	ParseXml();
-	~ParseXml();
-	char* GetRsaAttestKey(){
-		return kRsaAttestKey;
-	}
-	size_t GetRsaAttestKeySize(){
-		return kRsaAttestKeySize;
-	}
-	char* GetRsaAttestCert(){
-		return kRsaAttestCert;
-	}
-	size_t GetRsaAttestCertSize(){
-		return kRsaAttestCertSize;
-	}
-	char* GetRsaAttestRootCert(){
-		return kRsaAttestRootCert;
-	}
-	size_t GetRsaAttestRootCertSize(){
-		return kRsaAttestRootCertSize;
-	}
-	char* GetEcAttestKey(){
-		return kEcAttestKey;
-	}
-	size_t GetEcAttestKeySize(){
-		return kEcAttestKeySize;
-	}
-	char* GetEcAttestCert(){
-		return kEcAttestCert;
-	}
-	size_t GetEcAttestCertSize(){
-		return kEcAttestCertSize;
-	}
-	char* GetEcAttestRootCert(){
-		return kEcAttestRootCert;
-	}
-	size_t GetEcAttestRootCertSize(){
-		return kEcAttestRootCertSize;
-	}
-
-	char GetCertChainLength(){
-		return key_num;
-	}
+    ParseXml();
+    ~ParseXml();
+    void SaveXmlFiles(void);
 
 private:
-	bool read_xml_from_rpmb(void);
-	void get_new_line(void);
-	void get_elems_value(const char* begin,const char* end,char*value);
-	void get_elems_value(const char* begin,const char* end,char* value,size_t *len);
-	char *strXmlStart(char const *s1,char const *start,char const *end);
-	char *strXmlEnd(char const *s1,char const *start,char const *end);
-	bool get_cert_data(xml_elems_e type,const char* begin,const char* end);
-	void parse_xml_elems(void);
-	void switch_Attest_Context(void);
-	char *xml_data;
-	size_t xml_size;
-	char *elem;
-	char *elem_end;
-	char version[4];
-	char devID[64];
-	char key_num;
-	char ecc_name[8];
-	char rsa_name[8];
-	char ecc_key_f[8];
-	char ecc_cert_f[8];
-	char rsa_key_f[8];
-	char rsa_cert_f[8];
-	char ecc_cert_num;
-	char rsa_cert_num;
-	//unique_ptr<char>
-	char* kRsaAttestKey;
-	size_t kRsaAttestKeySize;
-	char* kRsaAttestCert;
-	size_t kRsaAttestCertSize;
-	char* kRsaAttestRootCert;
-	size_t kRsaAttestRootCertSize;
-	char* kEcAttestKey;
-	size_t kEcAttestKeySize;
-	char* kEcAttestCert;
-	size_t kEcAttestCertSize;
-	char* kEcAttestRootCert;
-	size_t kEcAttestRootCertSize;
-	char kCertificateChainLength;
+    bool read_xml_from_rpmb(void);
+    void get_new_line(void);
+    void get_elems_value(const char* begin,const char* end,char*value);
+    void get_elems_value(const char* begin,const char* end,char* value,size_t *len);
+    char *strXmlStart(char const *s1,char const *start,char const *end);
+    char *strXmlEnd(char const *s1,char const *start,char const *end);
+    bool GetItemsValue(xml_elems_e type,const char* begin,const char* end);
+    void parse_xml_elems(char type);
+    bool write_data2file( const char*, const char*);
+    void skip_sp(char* in,char*out);
+    char *xml_data;
+    char *elem;
+    char *key_elem;
+    char *elem_end;
+    size_t xml_size;
+    char AttestBuf[2048]={0};
+    size_t AttestSize=0;
+    unsigned int key_num=0;
 };
 
 #endif //KEYMASTER_PARSE_XML_H_
